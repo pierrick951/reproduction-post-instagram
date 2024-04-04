f2370e95b9a3cf3de96d7af7e6aa0bcab6004337
Pierrick Ber <bernard.pierrick0@gmail.com>
2024-04-04 16:16:52 -0400

    ajout du dossiers coponents

--- instapost/src/App.jsx
+++ instapost/src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card/Card.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <div>rien</div>
+    <div className='app'>
+      <Card/>
+    </div>
   )
 }
 
---
+++ instapost/src/components/Card/Card.jsx
@@ -0,0 +1,11 @@
+import Header from "./header/Header"
+// import 'Card.css'
+
+export default function Card() {
+  return (
+    <div className="containerCard">
+        voici la carte 
+      <Header/>
+    </div>
+  )
+}
