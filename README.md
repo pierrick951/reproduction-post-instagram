df8ea1f7678a824696a8a6afc172a7f546798621
Pierrick Ber <bernard.pierrick0@gmail.com>
2024-04-07 10:52:18 -0400

    mise a jour du composant

--- README.md
+++ README.md
@@ -10,8 +10,6 @@ This project aims to create a mini React component simulating a functional Insta
 - A mini burger menu 🍔 should also be integrated as shown in the model.
 - The number of likes should increment or decrement based on whether the heart is red or not.
 - Clicking "see more" should be functional.
-- Users should be able to leave a comment, which will appear on the screen under the post (without refreshing the page).
-
 ---
 
 Good luck to myself! 💪
--- instapost/src/components/Footer/Footer.jsx
+++ instapost/src/components/Footer/Footer.jsx
@@ -1,9 +1,30 @@
 import { useState } from "react"
-
-
-
+import './Footer.css'
 export default function Footer() {
-  return (
-    <div>Footer</div>
-  )
+    
+const [Click, setClick] = useState(true)
+
+function showcontent(){
+    if(Click === true){
+
+        setClick(false)
+    }else {
+        setClick(!false)
+    }
+}
+    return (
+        <div>
+           <p className="publication">
+          {Click ? (
+            <><span>etuve_</span>
+            Soon back to MTL ⚜️<small onClick={showcontent}> ...plus</small></>
+          ) : (
+            <> <span>etuve_</span> Soon back to MTL ⚜️<br/>
+            #Canada #Travel <small onClick={showcontent}>moins</small>
+            </>
+          )}
+        </p>
+        <p><span>Jhone_Doe</span> j'ai haaaaaate 🔥 </p>
+        </div>
+    )
 }
